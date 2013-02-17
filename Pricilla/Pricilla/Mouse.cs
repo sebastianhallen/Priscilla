b7622f7d3a6fff4c1a4dd127302511abc1f66584
Sebastian Hallén <sebastian.hallen@gmail.com>
2013-02-17 16:53:18 +0100

    no need to specify VirtualDesktop for MoveCursor, it's relative movement

--- Pricilla/Pricilla/Mouse.cs
+++ Pricilla/Pricilla/Mouse.cs
@@ -23,7 +23,7 @@
 
         public void MoveCursor(int dx, int dy)
         {
-            mouse_event(MouseInput.VirtualDesktop | MouseInput.Move, (uint)dx, (uint)dy, 0, new IntPtr());
+            mouse_event(MouseInput.Move, (uint)dx, (uint)dy, 0, new IntPtr());
         }
 
         public void LeftDown()
@@ -79,19 +79,19 @@
 
         private static class MouseInput
         {
-            public const uint Absolute = 0x8000;   //The dx and dy parameters contain normalized absolute coordinates. If not set, those parameters contain relative data
-            public const uint VirtualDesktop = 0x4000;   //Maps coordinates to the entire desktop. Must be used with MouseInput.Absolute. 
-            public const uint LeftDown = 0x0002;   //The left button is down.
-            public const uint LeftUp = 0x0004;   //The left button is up.
-            public const uint MiddleDown = 0x0020;   //The middle button is down.
-            public const uint MiddleUp = 0x0040;   //The middle button is up.
-            public const uint Move = 0x0001;   //Movement occurred.
-            public const uint RightDown = 0x0008;   //The right button is down.
-            public const uint RightUp = 0x0010;   //The right button is up.
-            public const uint Wheel = 0x0800;   //The wheel has been moved, if the mouse has a wheel. The amount of movement is specified in dwData
-            public const uint XDown = 0x0080;   //An X button was pressed.
-            public const uint XUp = 0x0100;   //An X button was released.
-            public const uint HWheel = 0x1000;   //The wheel button is tilted.
+            public const uint Absolute = 0x8000;        //The dx and dy parameters contain normalized absolute coordinates. If not set, those parameters contain relative data
+            public const uint VirtualDesktop = 0x4000;  //Maps coordinates to the entire desktop. Must be used with MouseInput.Absolute. 
+            public const uint LeftDown = 0x0002;        //The left button is down.
+            public const uint LeftUp = 0x0004;          //The left button is up.
+            public const uint MiddleDown = 0x0020;      //The middle button is down.
+            public const uint MiddleUp = 0x0040;        //The middle button is up.
+            public const uint Move = 0x0001;            //Movement occurred.
+            public const uint RightDown = 0x0008;       //The right button is down.
+            public const uint RightUp = 0x0010;         //The right button is up.
+            public const uint Wheel = 0x0800;           //The wheel has been moved, if the mouse has a wheel. The amount of movement is specified in dwData
+            public const uint XDown = 0x0080;           //An X button was pressed.
+            public const uint XUp = 0x0100;             //An X button was released.
+            public const uint HWheel = 0x1000;          //The wheel button is tilted.
         }
     }
 }
