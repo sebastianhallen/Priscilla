190753756272336dc4b8b3425c3a326c8c68b995
Sebastian Hallén <sebastian.hallen@gmail.com>
2013-05-15 21:49:42 +0200

    fluent interface for window finder

--- Priscilla/Priscilla/ApplicationWindowFinder.cs
+++ Priscilla/Priscilla/ApplicationWindowFinder.cs
@@ -9,14 +9,22 @@
         [DllImport("user32.dll", SetLastError = true)]
         private static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
 
-        IntPtr IApplicationWindowFinder.FindWindow(string windowClass, string windowTitle)
+        public IntPtr FindWindow(string windowClass, string windowTitle)
         {
             return FindWindowEx(IntPtr.Zero, IntPtr.Zero, windowClass, windowTitle);
         }
 
-        IntPtr IApplicationWindowFinder.FindWindow(IntPtr hwndParent, string windowClass, string windowTitle)
+        public IntPtr FindChildWindow(IntPtr hwndParent, string windowClass, string windowTitle)
         {
             return FindWindowEx(hwndParent, IntPtr.Zero, windowClass, windowTitle);
+        }
+    }
+
+    public static class FluentApplicationWindowFinder
+    {
+        public static IntPtr FindChildWindow(this IntPtr hwndParent, string windowClass, string windowTitle = null)
+        {
+            return new ApplicationWindowFinder().FindChildWindow(hwndParent, windowClass, windowTitle);
         }
     }
 }
